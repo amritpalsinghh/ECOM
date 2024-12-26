35f1ee7d54df346d177a1553c22e87004feff8a7
amrit <43542062+amritpalsinghh@users.noreply.github.com>
2024-12-26 06:08:25 +0530

    Sending data form add to cart button ( html page ) to api using vue Js
    Task 3.2

--- saulgadgets_3_12_5/saulgadgets/apps/store/api.py
+++ saulgadgets_3_12_5/saulgadgets/apps/store/api.py
@@ -1,15 +1,20 @@
+
+import json
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 from apps.cart.cart import Cart
 from .models import Product
 
 def api_add_to_cart(request):
+    data = json.loads(request.body)
     jsonresponse = {'success': True}
     #NOTE The data retrieved from request.POST is always a string.
-    product_id = request.POST.get('product_id')
-    update = request.POST.get('update')#If 'update' is not present, update will be set to None.
-    quantity = request.POST.get('quantity',1)#If 'quantity' is not present, the default value 1 will be used
+    product_id = data['product_id']
+    update = data['update']#If 'update' is not present, update will be set to None.
+    quantity = data['quantity']#If 'quantity' is not present, the default value 1 will be used 
     
+    print(data)
+    print(data['product_id'])
     cart = Cart(request)
     
     product = get_object_or_404(Product, pk=product_id)
